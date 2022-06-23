2f448c906dd5444aa0cdf70a08c63981c99e3528
josephxia <15906475@qq.com>
2022-06-23 15:23:22 +0800

    update

--- 02-server/02-template/index.js
+++ 02-server/02-template/index.js
@@ -1,18 +1,19 @@
-const koa = require('koa');
-const app = new koa();
-const router = require('koa-router')();
+const koa = require('koa')
+const router = require('koa-router')()
 const static = require('koa-static2')
-const views = require('koa-views');
+const views = require('koa-views')
+const app = new koa()
 app.use(views(__dirname + '/views', {
     extension: 'ejs'
-}));
+}))
+// /asssts/abc.png
 app.use(static('assets', __dirname + '/assets'))
-router.get('/', async (ctx) => {
+router.get('/', async ctx => {
     await ctx.render('index', {
-        name: 'Header',
+        name: '然叔博客',
         menus: ['Home', 'Event', 'Gallery', 'News', 'About', 'Contact'],
-    });
-});
+    })
+})
 app.use(router.routes())
 app.listen(3000, () => {
     console.log('Server at 3000')
