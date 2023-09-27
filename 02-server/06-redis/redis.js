417804fbca6a0990819221bd1cf5a676b5bcefd4
josephxia <15906475@qq.com>
2023-09-27 10:54:51 +0800

    feat: update redis

--- 02-server/06-redis/redis.js
+++ 02-server/06-redis/redis.js
@@ -1,14 +1,15 @@
 (async () => {
-    const { createClient } = require('redis');
+    const { createClient } = require('redis')
     const client = await createClient()
-        .on('error', err => console.log('Redis Client Error', err))
-        .connect();
+        .on('error', err => console.err('Redis Error', err))
+        .connect()
 
-    await client.set('key', 'value');
-    const value = await client.get('key');
-    console.log('key:', value)
+    await client.set('k1', 'josephxia')
 
-    const keys = await client.keys('*');
-    console.log('keys:', keys)
-    await client.disconnect();
+    const name = await client.get('k1')
+    console.log('name:', name)
+
+    const keys = await client.keys('*')
+    console.log('keys', keys)
+
 })()
--- 02-server/06-redis/session-redis.js
+++ 02-server/06-redis/session-redis.js
@@ -1,36 +1,30 @@
-
 const koa = require('koa')
 const app = new koa()
-const redisStore = require('koa-redis');
+const redisSotre = require('koa-redis')
 const Redis = require('ioredis')
-const redisClient = new Redis({});
+const redisClient = new Redis()
 
 const session = require('koa-session')
 
-// 签名key keys作用 用来对cookie进行签名
-app.keys = ['some secret'];
+app.keys = ['xxxxx']
 
 app.use(session({
-    key: 'kkb:sess',
-    store: redisStore({ redisClient }) // 此处可以不必指定
-}, app));
+    key: 'rrr:sess',
+    store: redisSotre({ redisClient })
+}, app))
 
 app.use(async (ctx, next) => {
+    // 观察Redis状态
     const keys = await redisClient.keys('*')
-    keys.forEach(async key =>
-        console.log(await redisClient.get(key))
-    )
+    keys.forEach(async key => console.log(key + ':' + await redisClient.get(key)))
     await next()
 })
 
-// 测试
-app.use(ctx => {
-    if (ctx.path === '/favicon.ico') return;
-    // 获取
-    let n = ctx.session.count || 0;
-    // 设置
-    ctx.session.count = ++n;
-    ctx.body = '第' + n + '次访问';
-});
+app.use(async (ctx, next) => {
+    if (ctx.path === '/favicon.ico') return
+    let n = ctx.session.count || 0
+    ctx.session.count = ++n
+    ctx.body = `第${n}访问`
+})
 
 app.listen(3000)
